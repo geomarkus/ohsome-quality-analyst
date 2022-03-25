b24999cdbd0ecabd6197c45ba95a0f373f0a4391
hn437 <Danner@stud.uni-heidelberg.de>
2022-03-25 12:33:53 +0100

    added further checks to testfunctions and adjusted existing ones in order to work with the new way of calculating the covariates in test_indicator_building_area.py

--- workers/tests/integrationtests/test_indicator_building_area.py
+++ workers/tests/integrationtests/test_indicator_building_area.py
@@ -16,35 +16,55 @@ from .utils import oqt_vcr
 
 class TestIndicatorBuildingArea(unittest.TestCase):
     def setUp(self):
-        # Heidelberg
+        # Touggourt, Algeria
         self.feature = asyncio.run(
-            db_client.get_feature_from_db(dataset="regions", feature_id="3")
+            db_client.get_feature_from_db(dataset="regions", feature_id="12")
         )
         self.layer_name = "building_area"
         self.indicator = BuildingArea(feature=self.feature, layer_name=self.layer_name)
 
     @oqt_vcr.use_cassette()
-    def test(self):
+    def test_indicator(self):
         asyncio.run(self.indicator.preprocess())
-        self.assertIsNotNone(self.indicator.area)
-        self.assertIsNotNone(self.indicator.building_area)
-        self.assertIsInstance(self.indicator.result.timestamp_osm, datetime)
+        self.assertIsNotNone(self.indicator.building_area_osm)
+        self.assertIsInstance(self.indicator.building_area_osm, list)
+        self.assertGreater(len(self.indicator.building_area_osm), 0)
+        self.assertIsInstance(self.indicator.result.timestamp_osm, list)
+        self.assertIsInstance(self.indicator.result.timestamp_osm[0], datetime)
         self.assertIsInstance(self.indicator.result.timestamp_oqt, datetime)
-        self.assertIsNotNone(self.indicator.attrdict["ghspop"])
-        self.assertIsNotNone(self.indicator.attrdict["ghspop_density_per_sqkm"])
-        self.assertIsNotNone(self.indicator.attrdict["water"])
-        self.assertIsNotNone(self.indicator.attrdict["very_low_rural"])
-        self.assertIsNotNone(self.indicator.attrdict["low_rural"])
-        self.assertIsNotNone(self.indicator.attrdict["rural_cluster"])
-        self.assertIsNotNone(self.indicator.attrdict["suburban"])
-        self.assertIsNotNone(self.indicator.attrdict["semi_dense_urban_cluster"])
-        self.assertIsNotNone(self.indicator.attrdict["dense_urban_cluster"])
-        self.assertIsNotNone(self.indicator.attrdict["urban_centre"])
-        self.assertIsNotNone(self.indicator.attrdict["shdi_mean"])
-        self.assertIsNotNone(self.indicator.attrdict["vnl_sum"])
-        self.assertLessEqual(self.indicator.attrdict["urban_centre"], 1)
-        self.assertGreaterEqual(self.indicator.attrdict["urban_centre"], 0)
-        self.assertRaises(KeyError, lambda: self.indicator.attrdict["some_random_key"])
+        self.assertIsNotNone(self.indicator.result.timestamp_oqt)
+        self.assertIsNotNone(self.indicator.covariates)
+        self.assertGreater(len(self.indicator.covariates), 0)
+        self.assertIsNotNone(self.indicator.covariates[0].ghs_pop)
+        self.assertIsNotNone(self.indicator.covariates[0].ghs_pop_density)
+        self.assertIsNotNone(self.indicator.covariates[0].water)
+        self.assertIsNotNone(self.indicator.covariates[0].very_low_density_rural)
+        self.assertIsNotNone(self.indicator.covariates[0].low_density_rural)
+        self.assertIsNotNone(self.indicator.covariates[0].rural_cluster)
+        self.assertIsNotNone(self.indicator.covariates[0].suburban_or_peri_urban)
+        self.assertIsNotNone(self.indicator.covariates[0].semi_dense_urban_cluster)
+        self.assertIsNotNone(self.indicator.covariates[0].dense_urban_cluster)
+        self.assertIsNotNone(self.indicator.covariates[0].urban_centre)
+        self.assertIsNotNone(self.indicator.covariates[0].shdi)
+        self.assertIsNotNone(self.indicator.covariates[0].vnl)
+        self.assertLessEqual(self.indicator.covariates[0].urban_centre, 1)
+        self.assertGreaterEqual(self.indicator.covariates[0].urban_centre, 0)
+        self.assertGreater(
+            (
+                self.indicator.covariates[0].urban_centre
+                + self.indicator.covariates[0].dense_urban_cluster
+                + self.indicator.covariates[0].semi_dense_urban_cluster
+                + self.indicator.covariates[0].suburban_or_peri_urban
+                + self.indicator.covariates[0].rural_cluster
+                + self.indicator.covariates[0].low_density_rural
+                + self.indicator.covariates[0].very_low_density_rural
+                + self.indicator.covariates[0].water
+            ),
+            0,
+        )
+        self.assertRaises(
+            AttributeError, lambda: self.indicator.covariates[0].some_random_key
+        )
 
         self.indicator.calculate()
         self.assertIsNotNone(self.indicator.result.label)
@@ -52,17 +72,14 @@ class TestIndicatorBuildingArea(unittest.TestCase):
         self.assertIsNotNone(self.indicator.result.description)
         self.assertLessEqual(self.indicator.result.value, 1.0)
         self.assertGreaterEqual(self.indicator.result.value, 0.0)
-        self.assertIsNotNone(self.indicator.result.label)
-        self.assertIsNotNone(self.indicator.result.description)
 
         self.indicator.create_figure()
         self.assertIsNotNone(self.indicator.result.svg)
 
-    # def test_get_ghs_pop_density(self):
-    #     result = asyncio.run(get_ghs_pop_density(self.feature))
-    #     self.assertEqual(result, 80.38435859331635)
-
     def test_get_smod_class_share(self):
+        self.feature = asyncio.run(
+            db_client.get_feature_from_db(dataset="regions", feature_id="3")
+        )
         result = get_smod_class_share(self.feature)
         self.assertDictEqual(result, {"urban_centre": 1.0})
 
@@ -73,6 +90,7 @@ class TestIndicatorBuildingArea(unittest.TestCase):
         )
         result = asyncio.run(select_hex_cells(self.feature.geometry))
         self.assertIsInstance(result, FeatureCollection)
+        self.assertIsNotNone(result.features)
 
 
 if __name__ == "__main__":
